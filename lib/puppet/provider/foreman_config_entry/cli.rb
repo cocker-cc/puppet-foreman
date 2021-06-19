279565dc5e18d7938e3c9862a4675d8dee969453
Cocker Koch <github@cocker.cc>
2021-06-20 00:37:31 +0200

    - let Provider foreman_config_entry accept Values of Type Sensitive

--- lib/puppet/provider/foreman_config_entry/cli.rb
+++ lib/puppet/provider/foreman_config_entry/cli.rb
@@ -65,6 +65,8 @@ Puppet::Type.type(:foreman_config_entry).provide(:cli) do
 
   def value=(value)
     return if resource[:dry]
+
+    value = value.unwrap if value.is_a?(Puppet::Pops::Types::PSensitiveType::Sensitive)
     run_foreman_config("-k '#{name}' -v '#{value}'", :combine => true, :failonfail => true)
     @property_hash[:value] = value
   end
