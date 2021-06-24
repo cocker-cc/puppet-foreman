b972cfeb74c932cce8368b681156a8b9b586c4b5
Cocker Koch <github@cocker.cc>
2021-06-24 21:28:27 +0200

    Sensitive for Function to_symbolized_yaml
    - let Function to_symbolized_yaml handle Input-Data with Elements of
      Datatype Sensitive
    - let Function to_symbolized_yaml return a YAML-String of Datatype
      Sensitive, if Input-Data contains Elements of Datatype Sensitive

--- lib/puppet/functions/foreman/to_symbolized_yaml.rb
+++ lib/puppet/functions/foreman/to_symbolized_yaml.rb
@@ -26,13 +26,61 @@ Puppet::Functions.create_function(:'foreman::to_symbolized_yaml') do
   dispatch :to_symbolized_yaml do
     param 'Any', :data
     optional_param 'Hash', :options
+    # this is only for Testing-Reasons, because Rspec cannot test sensitive Returnvalues
+    optional_param 'Boolean', :sensitive
   end
 
-  def to_symbolized_yaml(data, options = {})
-    if data.is_a?(Hash)
-      data = Hash[data.map { |k, v| [k.to_sym, v] }]
+  def to_symbolized_yaml(data, options = {}, sensitive = true)
+    # data eventually contains Elements of Datatype Sensitive
+    data_unsensitive = deep_unwrap(data)
+
+    # actually symbolize the Firstlevel-Keys
+    if data_unsensitive.is_a?(Hash)
+      data_unsensitive = Hash[data_unsensitive.map { |k, v| [k.to_sym, v] }]
     end
 
-    data.to_yaml(options)
+    # we return the YAML as Datatype Sensitive, if the Input contains Elements of Datatype Sensitive
+    if sensitive && has_sensitive_value(data)
+      Puppet::Pops::Types::PSensitiveType::Sensitive.new(data_unsensitive.to_yaml(options))
+    else
+      data_unsensitive.to_yaml(options)
+    end
+  end
+
+  ### Helper-Functions
+
+  def has_sensitive_value(data)
+    found = false
+    if data.is_a?(Hash)
+      data.each do |key, val|
+        found = true if has_sensitive_value(val)
+      end
+    elsif data.is_a?(Array)
+      data.each do |val|
+        found = true if has_sensitive_value(val)
+      end
+    elsif data.respond_to?(:unwrap)
+      found = true
+    else
+      # do nothing; keep current Value of Variable found
+    end
+
+    found
+  end
+
+  def deep_unwrap(data)
+    if data.is_a?(Hash)
+      data.map do |key, val|
+        [key, deep_unwrap(val)]
+      end.to_h
+    elsif data.is_a?(Array)
+      data.map do |val|
+        deep_unwrap(val)
+      end
+    elsif data.respond_to?(:unwrap)
+      data.unwrap
+    else
+      data
+    end
   end
 end
--- spec/functions/foreman_to_symbolized_yaml_spec.rb
+++ spec/functions/foreman_to_symbolized_yaml_spec.rb
@@ -5,7 +5,26 @@ describe 'foreman::to_symbolized_yaml' do
     is_expected.not_to eq(nil)
   end
 
-  it 'should symbolize keys' do
+  it 'should handle Hash and symbolize keys' do
     is_expected.to run.with_params({'a' => 'b'}).and_return("---\n:a: b\n")
+  end
+  it 'should handle Hash and symbolize keys only at first-level' do
+    is_expected.to run.with_params({'a' => 'A', 'b' => {'c' => 'C'}}).and_return("---\n:a: A\n:b:\n  c: C\n")
+  end
+  it 'should handle Array' do
+    is_expected.to run.with_params(['a', 'b']).and_return("---\n- a\n- b\n")
+  end
+
+  it 'should handle Hash and should unwrap Sensitive' do
+    is_expected.to run.with_params({'a' => sensitive('b')}, {}, false).and_return("---\n:a: b\n")
+  end
+  it 'should handle Array and should unwrap Sensitive' do
+    is_expected.to run.with_params(['a', sensitive('b')], {}, false).and_return("---\n- a\n- b\n")
+  end
+
+  # Test of a Returnvalue of Datatype Sensitive does not work
+  it 'should handle Hash, should unwrap Sensitive and return Sensitive' do
+    pending 'should have a Returnvalue of Datatype Sensitive'
+    is_expected.to run.with_params({'a' => sensitive('b')}).and_return(sensitive("---\n:a: b\n"))
   end
 end
